5d6a69715d3cfcccc44c012c22c40abe6e2f700b
Nabil080 <nbellila@student.42.fr>
2024-09-11 20:10:44 +0200

    precise usleep done

--- srcs/utils.c
+++ srcs/utils.c
@@ -6,7 +6,7 @@
 /*   By: nbellila <nbellila@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 20:50:01 by nbellila          #+#    #+#             */
-/*   Updated: 2024/09/11 19:57:50 by nbellila         ###   ########.fr       */
+/*   Updated: 2024/09/11 20:10:20 by nbellila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,10 +33,21 @@ time_t	get_current_time(void)
 	return ((tv.tv_sec * 1000) + (tv.tv_usec / 1000));
 }
 
-void	ft_usleep(time_t time)
+void	ft_usleep(time_t time_in_ms)
 {
 	time_t	start;
+	time_t	remaining;
 
 	start = get_current_time();
-	usleep(time);
+	remaining = time_in_ms;
+	while (remaining > 10)
+	{
+		usleep(remaining / 2 * 1000);
+		remaining = time_in_ms - (get_current_time() - start);
+	}
+	while (remaining > 0)
+	{
+		usleep(10);
+		remaining = time_in_ms - (get_current_time() - start);
+	}
 }
